d3526b5c30e78b2e29681b4ebad739bc79d1c7ac
John Furtado <jf8846@gmail.com>
2017-09-05 22:52:42 +0000

    follow along level 6

---
+++ level6/webSocket.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+   console.log('Client connected...');
+   
+   client.emit('messages', {hello: 'world'});
+   client.on('messages', function(data) {
+      console.log(data); 
+   });
+});
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/webSocket.html'); 
+});
+
+server.listen(process.env.PORT);
