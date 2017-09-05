2dc53409554bc185f429455cd15eec30fdd1252c
John Furtado <jf8846@gmail.com>
2017-09-05 22:50:36 +0000

    follow along level 4

---
+++ level4/make_request.js
@@ -0,0 +1,23 @@
+var http = require('http');
+
+var makeRequest = function(message) {
+
+    var options = {
+        host: 'localhost', port: 8080, path: '/', method: 'POST'
+    }
+    
+    var request = http.request(options, function(response) {
+        response.on('data', function(data) {
+            console.log(data.toString());    
+        });
+    });
+    request.on('error', function(err) {
+        // Handle error
+        console.log(err)
+    });
+    
+    request.write(message);
+    request.end();
+}
+
+module.exports = makeRequest;
